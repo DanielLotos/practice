e7d4cb0318e77dc57ff27511b3e430fde7eaa608
Denis Kratos <daniellika@yandex.ru>
2018-07-11 23:43:22 +0300

    swut: new accordion category | fix bug with attributes

--- src/js/showCategories.js
+++ src/js/showCategories.js
@@ -1,5 +1,19 @@
 // Call accordion
 $( "li.user__item--categories" ).click(function() {
   $( "div.hamburger" ).toggleClass( "is-active" ); // animation hamburger
-  $( "ul.user-categories" ).slideToggle("fast"); // active accordion
+  $( "ul.categories" ).slideToggle("fast"); // active accordion
 });
+
+// Function detect resize window
+$(function() {
+  $(window).resize(delAttClass);
+});
+
+// Cleaning extra attributs and classes
+function delAttClass() {
+  let widthVal = document.body.clientWidth + 15;
+  if (widthVal >= 1023) {
+    document.getElementsByClassName("categories")[0].removeAttribute("style");
+    document.getElementsByClassName("hamburger")[0].classList.remove("is-active");
+  }
+};
